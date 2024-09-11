3b90ace9a841896438910ca38e1536f753efa265
Abdulrafiudev <introvertedscholar@gmail.com>
2024-09-11 19:10:19 +0100

    completed withdrawal section

---
+++ js/withdrawals.js
@@ -0,0 +1,33 @@
+const withdrawButton = document.querySelector(".withdrawal_submit");
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".close_button");
+const amount = document.querySelector(".amountwithdraw input");
+const errorMessage = document.querySelector(".error_message");
+const cryptoWallet = document.querySelector(".deposit_body_details_cont input");
+
+amount.value = "";
+
+amount.addEventListener("input", () => {
+  if (amount.value.trim() == "") {
+    errorMessage.style.display = "block";
+  } else {
+    errorMessage.style.display = "none";
+  }
+});
+
+withdrawButton.addEventListener("click", () => {
+  if (amount.value.trim() == "") {
+    errorMessage.style.display = "block";
+  } else {
+    errorMessage.style.display = "none";
+    modal.classList.add("show");
+    document.body.style.overflowY = "hidden";
+    document.body.style.height = "100vh";
+  }
+});
+
+closeButton.addEventListener("click", () => {
+  modal.classList.remove("show");
+  document.body.style.overflowY = "scroll";
+  document.body.style.height = "100vh";
+});
