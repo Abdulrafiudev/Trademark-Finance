cbcb794a788f3c51e43c8ba6881578ea5a86de15
Abdulrafiudev <introvertedscholar@gmail.com>
2024-12-06 19:20:56 +0100

    new update

--- api/api.js
+++ api/api.js
@@ -1,4 +1,4 @@
-// const backendAPI =
-//   "https://trademarkfinnancebackend.onrender.com" ;
-const backendAPI = "http://localhost:3000";
+const backendAPI =
+  "https://trademarkfinnancebackend.onrender.com" ;
+// const backendAPI = "http://localhost:3000";
 export default backendAPI;
