58c49c4bcd651f5a7c872f7ea545517ad6d9dfb0
Abdulrafiu Ibrahim <introvertedscholar@gmail.com>
2025-10-15 10:18:34 +0100

    changes

--- api/api.js
+++ api/api.js
@@ -1,3 +1,3 @@
-const backendAPI = "https://trademarkfinnancebackend.onrender.com";
+const backendAPI = "https://trademarkfinancebackend.pxxl.click";
 // const backendAPI = "http://localhost:3000";
 export default backendAPI;
